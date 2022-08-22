b28fb2636441ee7be6ee0e176fec293d5b1ba39a
Pascal Iske <info@pascaliske.dev>
2022-08-22 09:47:31 +0200

    fix(docker-entrypoint): only connect sync dir if initial sync was successful

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,11 +7,13 @@ SYNC_DIR_PATH="${GIT_SYNC_ROOT:-/tmp}/${GIT_SYNC_DEST}/config"
 # ensure directories exist
 mkdir -p "${SYNC_DIR_PATH}" /config
 
-# one-time sync required for initial connection
-/sbin/git-sync --one-time
-
-# connect sync directory
-ln -sf "${SYNC_DIR_PATH}"/* /config
+# connect sync directory after initial one-time sync
+if /sbin/git-sync --one-time; then
+    echo "Connected: ${SYNC_DIR_PATH} -> /config"
+    ln -sf "${SYNC_DIR_PATH}"/* /config
+else
+    echo "Warning: Skipped linking due to failed git-sync command!"
+fi
 
 # exec container command
 if [ "${GIT_SYNC_ONE_TIME}" != 'true' ]; then
