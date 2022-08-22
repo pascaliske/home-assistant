cf860ff594cb4de43db6c18990eb7d4ee736b3e0
Pascal Iske <info@pascaliske.dev>
2022-08-22 10:11:03 +0200

    refactor(docker-entrypoint): add more logs to entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,15 +8,17 @@ SYNC_DIR_PATH="${GIT_SYNC_ROOT:-/tmp}/${GIT_SYNC_DEST}/config"
 mkdir -p "${SYNC_DIR_PATH}" /config
 
 # connect sync directory after initial one-time sync
+echo "==> Running initial sync for connection"
 if /sbin/git-sync --one-time; then
-    echo "Connected: ${SYNC_DIR_PATH} -> /config"
+    echo "==> Connected: ${SYNC_DIR_PATH} -> /config"
     ln -sf "${SYNC_DIR_PATH}"/* /config
 else
-    echo "Warning: Skipped linking due to failed git-sync command!"
+    echo "==> Warning: Skipped linking due to failed git-sync command!"
 fi
 
 # exec container command
 if [ "${GIT_SYNC_ONE_TIME}" != 'true' ]; then
+    echo "==> Continue execution"
     exec "$@"
 fi
 
