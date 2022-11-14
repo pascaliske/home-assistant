fc30c870cf6f27109560c4151bd5478286e9bea6
Pascal Iske <info@pascaliske.dev>
2022-11-14 11:26:10 +0100

    refactor(docker): only run one-time sync if explicitly requested

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,14 @@ SYNC_DIR_PATH="${GIT_SYNC_ROOT:-/tmp}/${GIT_SYNC_DEST}/config"
 mkdir -p "${SYNC_DIR_PATH}" /config
 
 # connect sync directory after initial one-time sync
-echo "==> Running initial sync for connection"
-if /sbin/git-sync --one-time; then
-    echo "==> Connected: ${SYNC_DIR_PATH} -> /config"
-    ln -sf "${SYNC_DIR_PATH}"/* /config
-else
-    echo "==> Warning: Skipped linking due to failed git-sync command!"
+if [ "${GIT_SYNC_ONE_TIME}" = 'true' ]; then
+    echo "==> Running initial sync for connection"
+    if /sbin/git-sync --one-time; then
+        echo "==> Connected: ${SYNC_DIR_PATH} -> /config"
+        ln -sf "${SYNC_DIR_PATH}"/* /config
+    else
+        echo "==> Warning: Skipped linking due to failed git-sync command!"
+    fi
 fi
 
 # exec container command
